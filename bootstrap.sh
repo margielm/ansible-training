ece17dd3ca8631a636a6563f3c0cc2edeeeb3a51
margielm <ewunia1234>
2017-07-04 16:40:39 +0200

    changes to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+apt-key update
 apt-get install software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
-apt-get install ansible -y
+apt-get install ansible -y --force-yes
 #acp /vagrant/hosts /etc/ansible
